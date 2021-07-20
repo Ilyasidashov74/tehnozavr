488b4fe398d4279e3bcd211ee884a8fafa0429af
Sidashov Ilya <Sidashov123@gmail.com>
2021-07-20 17:01:10 +0300

    'deploy'

---
+++ precache-manifest.25a87972cee1ae58aab7bdd16a0aac04.js
@@ -0,0 +1,406 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e0a8ebae0c5c4ddcfc7",
+    "url": "/tehnozavr/css/app.70e8ca46.css"
+  },
+  {
+    "revision": "2837ea65d35301889f2c8c3cb0ee6d3a",
+    "url": "/tehnozavr/css/style.min.css"
+  },
+  {
+    "revision": "a8b4ae57b1ac89c92b5a42062dd7187f",
+    "url": "/tehnozavr/fonts/PressStart2P-Regular.woff2"
+  },
+  {
+    "revision": "15a913e1228cca9b1247b373bdf27e55",
+    "url": "/tehnozavr/fonts/Roboto-Bold.woff2"
+  },
+  {
+    "revision": "ed4b08d2702fa26acc324ef1e89ae837",
+    "url": "/tehnozavr/fonts/Roboto-Light.woff2"
+  },
+  {
+    "revision": "50d01d3e6c994995bcaf829e63d53d1a",
+    "url": "/tehnozavr/fonts/Roboto-Medium.woff2"
+  },
+  {
+    "revision": "9feb0110b6dff9ee2b9ebd17f7a1aee6",
+    "url": "/tehnozavr/fonts/Roboto-Regular.woff2"
+  },
+  {
+    "revision": "fb040f49a4c74ae5ed8ea9a8cf33c63e",
+    "url": "/tehnozavr/img/airpods.jpg"
+  },
+  {
+    "revision": "d738ffda833d187f8f462dcb7c19a5e5",
+    "url": "/tehnozavr/img/airpods.webp"
+  },
+  {
+    "revision": "ec30662d01b9f1c155e18d3ebf310470",
+    "url": "/tehnozavr/img/airpods@2x.jpg"
+  },
+  {
+    "revision": "35dacf5c813262943d909f81a19fcdec",
+    "url": "/tehnozavr/img/airpods@2x.webp"
+  },
+  {
+    "revision": "5ffcfdc7b5b33edbb30731e6f0e27df4",
+    "url": "/tehnozavr/img/bicycle.jpg"
+  },
+  {
+    "revision": "fee1070c64b37f9cc2e2fd831e821a3e",
+    "url": "/tehnozavr/img/bicycle.webp"
+  },
+  {
+    "revision": "b17837abba7a9b08bd94c6fec3a8c84e",
+    "url": "/tehnozavr/img/bicycle@2x.jpg"
+  },
+  {
+    "revision": "12a6bcb5fe57f525934645ce5b17cf7b",
+    "url": "/tehnozavr/img/bicycle@2x.webp"
+  },
+  {
+    "revision": "fdabf5d4adee5f8b5d21db3e1fe9d24b",
+    "url": "/tehnozavr/img/board.jpg"
+  },
+  {
+    "revision": "45825c100f639c8509ea9733aba25470",
+    "url": "/tehnozavr/img/board.webp"
+  },
+  {
+    "revision": "875a0f2dc42013101f35ce8429007969",
+    "url": "/tehnozavr/img/board@2x.jpg"
+  },
+  {
+    "revision": "2b707ba5253c9366f4a1be58ae6c742e",
+    "url": "/tehnozavr/img/board@2x.webp"
+  },
+  {
+    "revision": "74b3abc76dfbc84fef5582cf18abf647",
+    "url": "/tehnozavr/img/dialog.jpg"
+  },
+  {
+    "revision": "73adcb9f48dcb09d95d508631e4d4c37",
+    "url": "/tehnozavr/img/divoom.jpg"
+  },
+  {
+    "revision": "5408b8843bca9887534f60980db3020a",
+    "url": "/tehnozavr/img/gorenje.jpg"
+  },
+  {
+    "revision": "10d9ba7ffac98bc2c57c490f4e4d0ca3",
+    "url": "/tehnozavr/img/harman.jpg"
+  },
+  {
+    "revision": "923267694fb4147b6ffb7d206faff335",
+    "url": "/tehnozavr/img/headphone@2x.jpg"
+  },
+  {
+    "revision": "8d2828467b8d5358959f80ee10af9181",
+    "url": "/tehnozavr/img/headphone@2x.webp"
+  },
+  {
+    "revision": "b3d5a068af3293e8445a18f81ec7f847",
+    "url": "/tehnozavr/img/headphones.jpg"
+  },
+  {
+    "revision": "566614a9951a05f386d3d8b2a11028cf",
+    "url": "/tehnozavr/img/headphones.webp"
+  },
+  {
+    "revision": "dc6dae452225db76e3ceab27a72b6aba",
+    "url": "/tehnozavr/img/jblgo.jpg"
+  },
+  {
+    "revision": "4f65670aec7998a668549c128307f51c",
+    "url": "/tehnozavr/img/mailru.png"
+  },
+  {
+    "revision": "3ad48a9c4125876c4775cb4b89e79b07",
+    "url": "/tehnozavr/img/panasonic.jpg"
+  },
+  {
+    "revision": "51a9254c0c282e96aa39f95fc71d5175",
+    "url": "/tehnozavr/img/phone-1@2x.jpg"
+  },
+  {
+    "revision": "1e78c832f7bf57ee9a9c6ff58aac3274",
+    "url": "/tehnozavr/img/phone-1@2x.webp"
+  },
+  {
+    "revision": "5a24c24163cb42b3acd0d7656498577c",
+    "url": "/tehnozavr/img/phone-2.jpg"
+  },
+  {
+    "revision": "3a63ec5ff1ace36ddf0752ee60f29d93",
+    "url": "/tehnozavr/img/phone-2.webp"
+  },
+  {
+    "revision": "f1b324d9900ca413525153daee1c4607",
+    "url": "/tehnozavr/img/phone-2@2x.jpg"
+  },
+  {
+    "revision": "cfb8071d4ecc905a4adacf2c557a2441",
+    "url": "/tehnozavr/img/phone-2@2x.webp"
+  },
+  {
+    "revision": "a0627054bd0c5dc1b4b3049ad9631344",
+    "url": "/tehnozavr/img/phone-3.jpg"
+  },
+  {
+    "revision": "b70e29dfa45a4bea80e2ab25920eee5f",
+    "url": "/tehnozavr/img/phone-3.webp"
+  },
+  {
+    "revision": "a6e5eefe9ebf715207fe52841a74419d",
+    "url": "/tehnozavr/img/phone-3@2x.jpg"
+  },
+  {
+    "revision": "7828763f3ac46a7f70c93016f321d268",
+    "url": "/tehnozavr/img/phone-3@2x.webp"
+  },
+  {
+    "revision": "9b7611512e498f3ab365418b23b46b50",
+    "url": "/tehnozavr/img/phone-square-1.jpg"
+  },
+  {
+    "revision": "29aa8c34f1fb95c879b8ea5addc71500",
+    "url": "/tehnozavr/img/phone-square-1.webp"
+  },
+  {
+    "revision": "04ae3bee97896288da7c203989a512b8",
+    "url": "/tehnozavr/img/phone-square-1@2x.jpg"
+  },
+  {
+    "revision": "fd01f52ba70c7b461c7357dce671831d",
+    "url": "/tehnozavr/img/phone-square-1@2x.webp"
+  },
+  {
+    "revision": "e599b059e7a8fd8793c703a76dc89f7d",
+    "url": "/tehnozavr/img/phone-square-2.jpg"
+  },
+  {
+    "revision": "694122786fca6a83e1921f7b41ee9c1e",
+    "url": "/tehnozavr/img/phone-square-2.webp"
+  },
+  {
+    "revision": "b8fc5e3285419146c5e04c46b057184f",
+    "url": "/tehnozavr/img/phone-square-2@2x.jpg"
+  },
+  {
+    "revision": "b97e5a53db806a2a88d72d428cda02da",
+    "url": "/tehnozavr/img/phone-square-2@2x.webp"
+  },
+  {
+    "revision": "b43ad3f8545adda1339b067ab7e557f8",
+    "url": "/tehnozavr/img/phone-square-3.jpg"
+  },
+  {
+    "revision": "f5322f3d79dcbe2c41de45137d0f09e3",
+    "url": "/tehnozavr/img/phone-square-3.webp"
+  },
+  {
+    "revision": "b72d8918de6c3448a38a3452d6ed7958",
+    "url": "/tehnozavr/img/phone-square-3@2x.jpg"
+  },
+  {
+    "revision": "7aff4884d869ff8eecd6392f91598a0e",
+    "url": "/tehnozavr/img/phone-square-3@2x.webp"
+  },
+  {
+    "revision": "c1f5287359ea715fd3d08dcca69ab304",
+    "url": "/tehnozavr/img/phone-square-4.jpg"
+  },
+  {
+    "revision": "5c4d98e04c40d89f2df6aa91781792fa",
+    "url": "/tehnozavr/img/phone-square-4.webp"
+  },
+  {
+    "revision": "05ca5546f0f50ae43ead3b07d2d57fa1",
+    "url": "/tehnozavr/img/phone-square-4@2x.jpg"
+  },
+  {
+    "revision": "f55cb4eb16f4191cc9ad95a4118258d8",
+    "url": "/tehnozavr/img/phone-square-4@2x.webp"
+  },
+  {
+    "revision": "17e352a4add586070d4f1d08e127ea4f",
+    "url": "/tehnozavr/img/phone-square.jpg"
+  },
+  {
+    "revision": "f7924b91736599a7fc0ae7e3cfe6bd0b",
+    "url": "/tehnozavr/img/phone-square.webp"
+  },
+  {
+    "revision": "d64a6665bfa72bdd6bf9aff054aedf14",
+    "url": "/tehnozavr/img/phone-square@2x.jpg"
+  },
+  {
+    "revision": "c97bc6013087a1f8d4efa91dfdb50e14",
+    "url": "/tehnozavr/img/phone-square@2x.webp"
+  },
+  {
+    "revision": "26a5e56b7c858602e9d4ea3a93befba4",
+    "url": "/tehnozavr/img/phone.jpg"
+  },
+  {
+    "revision": "da618a279fb27e4aff7cf158670db590",
+    "url": "/tehnozavr/img/phone.webp"
+  },
+  {
+    "revision": "083fb371c01693b7041a200e0623c0a0",
+    "url": "/tehnozavr/img/pic-square-2.jpg"
+  },
+  {
+    "revision": "3acaa889555a60549edee76f654e21b1",
+    "url": "/tehnozavr/img/pic-square-2.webp"
+  },
+  {
+    "revision": "ca5eae7790351cd909b3dcce3d867404",
+    "url": "/tehnozavr/img/pic-square-2@2x.jpg"
+  },
+  {
+    "revision": "04c576a9bf7baef36e11e6af4d7f4368",
+    "url": "/tehnozavr/img/pic-square-2@2x.webp"
+  },
+  {
+    "revision": "119be0228f6a880a336f55def3769c0b",
+    "url": "/tehnozavr/img/pic-square-3.jpg"
+  },
+  {
+    "revision": "3ce0c9c8451f9889c146a8c42104687c",
+    "url": "/tehnozavr/img/pic-square-3.webp"
+  },
+  {
+    "revision": "c1838cde8c887254bedf1012c24af194",
+    "url": "/tehnozavr/img/pic-square-3@2x.jpg"
+  },
+  {
+    "revision": "ebcc00ba950afc67dd9d8f6eb1bcb9e8",
+    "url": "/tehnozavr/img/pic-square-3@2x.webp"
+  },
+  {
+    "revision": "b6ec8af84e668cfc8d079513fbb46608",
+    "url": "/tehnozavr/img/radio.jpg"
+  },
+  {
+    "revision": "629723302c516ec3d3e318fe1e45fdc9",
+    "url": "/tehnozavr/img/radio.webp"
+  },
+  {
+    "revision": "f92911c428cf00f50ce33d348ff86ef7",
+    "url": "/tehnozavr/img/radio@2x.jpg"
+  },
+  {
+    "revision": "75520f02ee4eda7d617917ee188ce59d",
+    "url": "/tehnozavr/img/radio@2x.webp"
+  },
+  {
+    "revision": "a31c6b339aed56cf0117af94ec7d31fc",
+    "url": "/tehnozavr/img/ripstick@2x.jpg"
+  },
+  {
+    "revision": "0bc1f0af40bb5ab84db23f0154baa0b1",
+    "url": "/tehnozavr/img/ripstick@2x.webp"
+  },
+  {
+    "revision": "564b80a22ae096c33a1ed34fb6c56c9a",
+    "url": "/tehnozavr/img/ripstik.jpg"
+  },
+  {
+    "revision": "22c3e663f20cf4bc52a1fd438e16decf",
+    "url": "/tehnozavr/img/ripstik.webp"
+  },
+  {
+    "revision": "983bb10134d6bdf36993d5b4fca32b89",
+    "url": "/tehnozavr/img/rombica.jpg"
+  },
+  {
+    "revision": "82a8838b2043c1d7a3ea8e163634e1b6",
+    "url": "/tehnozavr/img/scooter.jpg"
+  },
+  {
+    "revision": "0c85b681ea849c5ba9e7b214bdc7fcef",
+    "url": "/tehnozavr/img/scooter.webp"
+  },
+  {
+    "revision": "1d0ff912f3d5fe7b19793faf8875db85",
+    "url": "/tehnozavr/img/scooter@2x.jpg"
+  },
+  {
+    "revision": "85cd5070993346b594b426216678e859",
+    "url": "/tehnozavr/img/scooter@2x.webp"
+  },
+  {
+    "revision": "649b463fa53a10884a4f3affa856af54",
+    "url": "/tehnozavr/img/svg/icon-arrow-bottom.svg"
+  },
+  {
+    "revision": "08607d27be3437dafa98c30f5bd356e3",
+    "url": "/tehnozavr/img/svg/logo-tech.svg"
+  },
+  {
+    "revision": "e35ca69a8152eaced0efa4b5c348a738",
+    "url": "/tehnozavr/img/svg/sprite.svg"
+  },
+  {
+    "revision": "c5f82ac71f200d9b6056d65aba0505cc",
+    "url": "/tehnozavr/img/toothbrush.jpg"
+  },
+  {
+    "revision": "96113e0205b3ac421916e07777cf0dc6",
+    "url": "/tehnozavr/img/toothbrush.webp"
+  },
+  {
+    "revision": "c8462a92c8d88325e0e9b0162b2a3ae9",
+    "url": "/tehnozavr/img/toothbrush@2x.jpg"
+  },
+  {
+    "revision": "5a5f1c56444418dfe7e94d2741c3505e",
+    "url": "/tehnozavr/img/toothbrush@2x.webp"
+  },
+  {
+    "revision": "d8618d6ed2b6ee19c20ba2f2e8fd0e27",
+    "url": "/tehnozavr/img/tronsmart.jpg"
+  },
+  {
+    "revision": "1abf1a850a9d70c1a8622b519e4436b5",
+    "url": "/tehnozavr/img/ural.jpg"
+  },
+  {
+    "revision": "dfc86818368445c0974e6fa2c2003093",
+    "url": "/tehnozavr/img/wheels.jpg"
+  },
+  {
+    "revision": "e017569e372fd6ba44b0e5ab17b5863b",
+    "url": "/tehnozavr/img/wheels.webp"
+  },
+  {
+    "revision": "76274e39827708a109a49294b583e827",
+    "url": "/tehnozavr/img/wheels@2x.jpg"
+  },
+  {
+    "revision": "f9eb5b380f300d2bacaa76720b9365b1",
+    "url": "/tehnozavr/img/wheels@2x.webp"
+  },
+  {
+    "revision": "14dc3925903ad03a9ec5618d1414f32a",
+    "url": "/tehnozavr/index.html"
+  },
+  {
+    "revision": "2e0a8ebae0c5c4ddcfc7",
+    "url": "/tehnozavr/js/app.aacdad8f.js"
+  },
+  {
+    "revision": "cf6749cf8aea0a4430ab",
+    "url": "/tehnozavr/js/chunk-vendors.036228b6.js"
+  },
+  {
+    "revision": "ad9c3ffd46dd862740cccdc5ebadd3b1",
+    "url": "/tehnozavr/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/tehnozavr/robots.txt"
+  }
+]);
